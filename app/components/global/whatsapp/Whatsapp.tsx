b51f1837dda39a005f0801b3932861484c6603b5
Oséias Costa <oseiasc2@gmail.com>
2023-10-30 17:48:29 +0000

    feat(Whatsapp): create whatsapp component

---
+++ app/components/global/whatsapp/Whatsapp.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import WhatsSVG from "../../../../public/images/global/whatsapp-button.svg";
+import { Whats, WhatsIcon } from "./whatsapp.styles";
+
+export default function Whatsapp() {
+  return (
+    <Whats>
+      <a href="https://nux514f6cjp.typeform.com/to/itgZsLcm" target="_blank">
+        <WhatsIcon src={WhatsSVG} alt="Link para o WhatsApp Mad Digital" />
+      </a>
+    </Whats>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import About from "./components/about/About";
 import Benefits from "./components/benefits/Benefits";
 import Call from "./components/call/Call";
 import Film from "./components/film/Film";
+import Whatsapp from "./components/global/whatsapp/Whatsapp";
 import Headline from "./components/headline/Headline";
 import Services from "./components/services/Services";
-
 
 export default function Home() {
   return (
@@ -15,6 +15,7 @@ export default function Home() {
       <Film />
       <Call />
       <About />
+      <Whatsapp />
     </main>
-  )
+  );
 }
